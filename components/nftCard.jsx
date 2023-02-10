5a5b0c4c9bca4a55694d4b0c290fabecfe92b91a
Sakal05 <samnang.sakal@gmail.com>
2023-02-10 16:10:15 +0700

    fix box size for card

--- components/nftCard.jsx
+++ components/nftCard.jsx
@@ -2,20 +2,23 @@ export const NFTCard = ({ nft }) => {
   return (
     <div className="w-1/4 flex flex-col ">
       <div className="rounded-md">
+        <a href="">
         <img
           className="object-cover h-128 w-full rounded-t-md"
           src={nft.media[0].gateway}
         ></img>
+        </a>
       </div>
       <div className="flex flex-col y-gap-5  px-2 py-3 bg-slate-100 rounded-b-md h-130 ">
         <div className="">
           <h2 className="text-xl text-gray-800">{nft.title}</h2>
-          <p className="text-gray-600">Id: {nft.id.tokenId}</p>
-          <p className="text-gray-600">{nft.contract.address}</p>
+          <p className="text-gray-600 text-ellipsis	overflow-hidden">Id: {nft.id.tokenId}</p>
+          {/* <p className="text-gray-600">{nft.contract.address}</p> */}
         </div>
 
         <div className="flex-grow mt-2">
-          <p className="text-gray-600">{nft.description}</p>
+          <p className="text-gray-600 text-ellipsis	overflow-hidden ">{nft.description}</p>
+          
         </div>
       </div>
     </div>
